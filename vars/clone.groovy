5e33a356ab6a1afad62dcf0b1275a60d3dd7b4a0
vishvas patel <110728766+vishvas1410@users.noreply.github.com>
2026-02-02 23:41:03 +0530

    add files via upload

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,25 +1,3 @@
-
-def call(Map config = [:]) {
-
-    def branch = config.get('branch', 'main')
-    def repoUrl = config.get('repoUrl')
-
-    if (!repoUrl) {
-        error "❌ repoUrl is required for clone.groovy"
-    }
-
-    echo 'Cloning source code...'
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        userRemoteConfigs: [[
-            url: repoUrl
-        ]],
-        extensions: [
-            [$class: 'CloneOption', shallow: true, depth: 1]
-        ]
-    ])
-
-    echo '✅ Code cloned successfully.'
+def call(String url, String branch){
+  git url: "${url}", branch: "${branch}"
 }
